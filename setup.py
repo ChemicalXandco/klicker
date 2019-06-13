61ed4f3108705d298bc27f1272a136c587471e02
ChemicalXandco <jimjoh41@gmail.com>
2019-06-13 21:37:07 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,42 @@
+import sys, os, platform
+from cx_Freeze import setup, Executable
+
+PYTHON_INSTALL_DIR = os.path.dirname(os.path.dirname(os.__file__))
+os.environ['TCL_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tcl8.6')
+os.environ['TK_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tk8.6')
+
+base = None
+name = None
+bdist_msi_options = None
+if sys.platform == 'win32':
+    base = 'Win32GUI'
+    name = 'Simple Clicker.exe'
+    shortcut_table = [
+        ('DesktopShortcut',        # Shortcut
+         'DesktopFolder',          # Directory_
+         'Simple Clicker',         # Name
+         'TARGETDIR',              # Component_
+         '[TARGETDIR]'+name,       # Target
+         None,                     # Arguments
+         'a simple clicker tool to click quickly/constantly',# Description
+         None,                     # Hotkey
+         None,                     # Icon
+         None,                     # IconIndex
+         None,                     # ShowCmd
+         'TARGETDIR'               # WkDir
+         )]
+    msi_data = {"Shortcut": shortcut_table}
+    bdist_msi_options = {'data': msi_data}
+    
+opts = {'include_files':['config.ini', 'LICENSE', 'README.MD',
+                         'icon.ico',
+                         (os.path.join(PYTHON_INSTALL_DIR, 'DLLs', 'tcl86t.dll'), os.path.join('lib', 'tcl86t.dll')),
+                         (os.path.join(PYTHON_INSTALL_DIR, 'DLLs', 'tk86t.dll'), os.path.join('lib', 'tk86t.dll'))]}
+
+setup(  name = 'Simple Clicker',
+        version = '0.1',
+        description = 'a simple clicker tool to click quickly/constantly',
+        options = {'build_exe': opts, "bdist_msi": bdist_msi_options},
+        executables = [Executable('main.py', targetName=name,
+                                  icon='icon.ico',
+                                  base=base)])
