5a36f9ed85ded15edbf5ee1bd7df7c715db1ff27
ChemicalXandco <jimjoh41@gmail.com>
2019-06-16 21:11:57 +0100

    GUI implementation of dynamic settings

--- main.py
+++ main.py
@@ -11,31 +11,25 @@ class GUI:
         master.iconbitmap('icon.ico')
         master.geometry("200x200")
 
-        self.options = LabelFrame(master, text='Options')
-        self.options.pack()
-
-        self.hotkeyLabel = Label(self.options, text='Hotkey')
+        self.hotkeyLabel = Label(master, text='Hotkey')
         vcmd = (master.register(self.limitChar), '%i')
-        self.hotkey = Entry(self.options, validate='key', validatecommand=vcmd, width=2)
+        self.hotkey = Entry(master, validate='key', validatecommand=vcmd, width=2)
         self.hotkeyLabel.grid(row=0, column=0)
         self.hotkey.grid(row=0, column=1)
 
-        self.choices = ['left', 'right']
-        self.choice = StringVar(master)
-        self.choice.set(self.choices[0])
-        self.mouseClickLabel = Label(self.options, text='Mouse Button')
-        self.mouseClick = OptionMenu(self.options, self.choice, *self.choices)
-        self.mouseClickLabel.grid(row=1, column=0)
-        self.mouseClick.grid(row=1, column=1)
+        self.options = LabelFrame(master, text='Options')
+        self.options.grid(row=1, column=0, columnspan=2)
 
-        self.refreshButton = Button(self.options, text='Refresh Options From File', command=self.readSetting)
-        self.refreshButton.grid(row=2, column=1)
+        self.optionWrapper(self.options, 'holdClick').pack()
 
-        self.saveButton = Button(self.options, text='Save Options To File', command=self.writeSetting)
-        self.saveButton.grid(row=3, column=1)
+        self.refreshButton = Button(master, text='Refresh Options From File', command=self.readSetting)
+        self.refreshButton.grid(row=2, column=0, columnspan=2)
+
+        self.saveButton = Button(master, text='Save Options To File', command=self.writeSetting)
+        self.saveButton.grid(row=3, column=0, columnspan=2)
 
         self.error = Label(master, text='', fg='#ff0000', wraplengt=master.winfo_width())
-        self.error.pack()
+        self.error.grid(row=4, column=0, columnspan=2)
 
         self.readSetting()
 
@@ -45,17 +39,25 @@ class GUI:
         else:
             return True
 
+    def optionWrapper(self, master, option):
+        frame = LabelFrame(master, text=option)
+
+        deleteButton = Button(frame, text='❌', command=lambda: frame.destroy())
+        deleteButton.grid(row=0, column=1)
+
+        options.optDict.get(option).Widget(frame, 2)
+        
+        return frame
+
     def readSetting(self):
         f = open('config.ini', 'r')
         self.hotkey.delete(0, END)
         self.hotkey.insert(0, f.readline().strip())
-        self.choice.set(f.readline().strip())
         f.close
 
     def writeSetting(self):
         f = open('config.ini', 'w')
         f.write(self.hotkey.get()+'\n')
-        f.write(self.choice.get())
         f.close()
 
 root = Tk()
