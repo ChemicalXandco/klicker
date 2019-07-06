437c0ce30ed71f66d5af47807c76819ffaada266
ChemicalXandco <jimjoh41@gmail.com>
2019-07-06 09:25:49 +0100

    add singleClick option

--- options/__init__.py
+++ options/__init__.py
@@ -1,5 +1,7 @@
-from options import holdClick, scroll
+from options import holdClick, scroll, singleClick
 
-optDict = {'holdClick': holdClick, 'scroll': scroll}
+optDict = {'holdClick': holdClick,
+           'scroll': scroll,
+           'singleClick':singleClick}
 
 optList = list(optDict.keys())
