d9e99cf120c3856a220ac20f13cda69b1f8fb68a
ChemicalXandco <jimjoh41@gmail.com>
2019-08-27 14:18:32 +0100

    split options into sequential and nonsequential

---
+++ options/nonsequential/__init__.py
@@ -0,0 +1,8 @@
+from options.nonsequential import (
+holdClick
+)
+
+optDict = {'holdClick': holdClick
+           }
+
+optList = list(optDict.keys())
---
+++ options/sequential/__init__.py
@@ -0,0 +1,10 @@
+from options.sequential import (
+scroll,
+singleClick
+)
+
+optDict = {'scroll': scroll,
+           'singleClick': singleClick
+           }
+
+optList = list(optDict.keys())
