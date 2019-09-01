081c7b831cbc1d8563f3f17f6273ed5973648630
ChemicalXandco <jimjoh41@gmail.com>
2019-09-01 21:37:42 +0100

    add keyDown and keyUp options

---
+++ options/sequential/keyDown.py
@@ -0,0 +1,24 @@
+from tkinter import *
+from time import sleep
+import pyautogui
+
+class Widget:
+    def __init__(self, parent, spacing):
+        self.parent = parent
+
+        self.key = Entry(parent, width=2)
+        self.key.grid(row=0, column=spacing)
+
+        self.labelOne = Label(parent, text='key pressed')
+        self.labelOne.grid(row=0, column=spacing+1, sticky=W)
+
+    def run(self):
+        pyautogui.keyDown(self.key.get()) 
+
+    def returnSettings(self):
+        settings = {}
+        settings['key'] = self.key.get()
+        return settings
+
+    def addSettings(self, settings):
+        self.key.set(settings['key'])
